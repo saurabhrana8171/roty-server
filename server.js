e4a1519c628626e3fc5d3e83470d906b68010167
saurabh.ranasa <saurabhranasa@graychaindesgin.com>
2023-12-01 20:39:07 +0530

    xyz

--- server.js
+++ server.js
@@ -10,24 +10,6 @@ const NODE_ENV = process.env.NODE_ENV || "production";
 require('dotenv').config({ path: '.env.' + NODE_ENV });
 const PORT = process.env.PORT
 app.use(cors());
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://rotyseven.com');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 app.use(express.urlencoded({ extended: false }));
 
 
